29e3adcaa9a9ff6249bb62f669b7b4a31c75262d
Johnny Awesome <35241451+johnnyawesome@users.noreply.github.com>
2023-10-23 16:23:18 +0200

    Cleaned up the code & added comments

--- DiffusionLimitedAggregation/JS/DiffusionLimitedAggregation.js
+++ DiffusionLimitedAggregation/JS/DiffusionLimitedAggregation.js
@@ -8,11 +8,14 @@ const maxCurrentWalkers = 100;
 const maxNumberOfFrozenWalkers = 1000;
 
 function setup() {
+
   angleMode(DEGREES);
   createCanvas(650, 650, P2D);
   background(0);
   fill(0, 255, 0);
   noStroke();
+
+  //Initialize the walkers
   initWalkers(maxCurrentWalkers);
 }
 
@@ -20,20 +23,27 @@ function draw() {
 
   background(0);
 
+  //Go through all walkers and...
   for (const walker of walkers) {
+    //...calculate their new position...
     walker.calcNextPos();
+    //...check if they get frozen...
     walker.freezeCheck();
+    //...and finally display them
     walker.display();
   }
 
+  //Adds a new walker when a walker gets frozen
   if (walkers.length < 1000) addNewWalkers();
 }
 
+//Initialize the walkers
 function initWalkers(numOfWalkers) {
 
   for (let i = 0; i < numOfWalkers; i++) walkers.push(new RandomWalker());
 }
 
+//Adds a new walker when a walker gets frozen
 function addNewWalkers() {
 
   const currentNumberOfFrozenWalkers = walkers.filter(w => !w.frozen).length
@@ -41,6 +51,7 @@ function addNewWalkers() {
     && currentNumberOfFrozenWalkers < maxNumberOfFrozenWalkers) walkers.push(new RandomWalker());
 }
 
+//Resets the animation
 function mouseClicked() {
   walkers = [];
   initWalkers(maxCurrentWalkers);
--- DiffusionLimitedAggregation/JS/RandomWalker.js
+++ DiffusionLimitedAggregation/JS/RandomWalker.js
@@ -5,32 +5,30 @@ class RandomWalker {
     this.position = walkers.length ? createVector(random() > 0.5 ? width : 0, random() > 0.5 ? height : 0) : createVector(width / 2, height / 2);
     this.noiseSeedX = Math.floor(random(10000000));
     this.noiseSeedY = Math.floor(random(10000000));
-
     this.frozen = walkers.length ? false : true;
   }
 
+  //Display the walker
   display() {
-
-    //if (!this.frozen) return;
     ellipse(this.position.x, this.position.y, walkerSize);
   }
 
+  //Calculate the walker's next position
   calcNextPos() {
 
     if (this.frozen) return;
 
-    this.noiseSeedX += 0.01;
-    this.noiseSeedY += 0.01;
+    this.position.x += noise(this.noiseSeedX += 0.01) * 30 - 15;
+    this.position.y += noise(this.noiseSeedY += 0.01) * 30 - 15;
 
-    this.position.x += noise(this.noiseSeedX) * 30 - 15;
-    this.position.y += noise(this.noiseSeedY) * 30 - 15;
-
+    //Wrap around the edges of the canvas
     if (this.position.x > width) this.position.x = 0;
     if (this.position.y > height) this.position.y = 0;
     if (this.position.x < 0) this.position.x = width;
     if (this.position.y < 0) this.position.y = height;
   }
 
+  //Check if the walker gets frozen
   freezeCheck() {
 
     for (const walker of walkers) {
